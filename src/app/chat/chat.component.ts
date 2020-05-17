9324661593b9489db9e3e6fa0545d08b8c617a1d
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-05-17 12:47:03 +0530

    Scroll to bottom on every new chat, Showing different styles for me/him message

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,36 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ChatService } from "./chat.service";
 import { ChatMessage } from "../model/chat.message";
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+    @ViewChild('chatArea', {static: false}) private myScrollContainer: ElementRef;
 
-  chatService: ChatService;
-  msgs = [];
-  conversation: string;
+    chatService: ChatService;
+    msgs = [];
+    conversation: string;
 
-  ngOnInit() {
-    this.chatService = new ChatService(this);
-    this.connect();
-  }
+    ngOnInit() {
+        this.chatService = new ChatService(this);
+        this.connect();
+    }
 
-  connect() {
-    this.chatService.connect();
-  }
+    connect() {
+        this.chatService.connect();
+    }
 
-  disconnect() {
-    this.chatService.disconnect();
-  }
+    disconnect() {
+        this.chatService.disconnect();
+    }
 
-  sendMessage() {
-    this.chatService.send(new ChatMessage(this.conversation, false, "DemoUser", new Date(), 'CHAT'));
-  }
+    sendMessage() {
+        this.chatService.send(new ChatMessage(this.conversation, false, "DemoUser", new Date(), 'CHAT'));
+    }
 
-  handleMessage(message: ChatMessage) {
-    this.msgs.push(message);
-  }
+    handleMessage(message: ChatMessage) {
+        this.msgs.push(message);
+    }
+
+    ngAfterViewChecked() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom(): void {
+        this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    }
 }
