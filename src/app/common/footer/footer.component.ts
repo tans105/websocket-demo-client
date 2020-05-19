a05fe900bdbb9e8202f8db63ddf557e7310ee609
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-05-20 02:04:47 +0530

    Added a footer component for all the pages

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FooterComponent {
+    date = new Date().getFullYear();
 }
